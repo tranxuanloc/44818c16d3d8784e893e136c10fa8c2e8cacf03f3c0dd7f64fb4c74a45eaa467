4de098936dae8d8c792c6c6f141d361f14b608f7
tranxuanloc <tranxuanloc.py@gmail.com>
2016-07-27 14:43:26 +0700

    Refactor module ChupHinh

---
+++ app/src/main/java/com/scsvn/whc_2016/main/Test.java
@@ -0,0 +1,7 @@
+package com.scsvn.whc_2016.main;
+
+/**
+ * Created by tranxuanloc on 7/13/2016.
+ */
+public class Test {
+}
---
+++ app/src/main/java/com/scsvn/whc_2016/main/attachmentImage/AttachmentImageFragment.java
@@ -0,0 +1,76 @@
+package com.scsvn.whc_2016.main.attachmentImage;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import com.scsvn.whc_2016.R;
+
+
+public class AttachmentImageFragment extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public AttachmentImageFragment() {
+        // Required empty public constructor
+    }
+    public static AttachmentImageFragment newInstance(String param1, String param2) {
+        AttachmentImageFragment fragment = new AttachmentImageFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_attachment_image, container, false);
+        GridView gridAttachmentImage = (GridView) view.findViewById(R.id.gridAttachmentImage);
+        return view;
+    }
+
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+}
